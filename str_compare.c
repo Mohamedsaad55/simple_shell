68ec6e37b92176598788447aee5043ab3c4863df
Heba elborie <hobalittleprinces@hotmail.com>
2023-05-17 12:49:00 -0700

    Update

--- str_compare.c
+++ str_compare.c
@@ -1,23 +1,23 @@
 #include "shell.h"
 /**
  * _str_compare - Compares two strings 
- * @s1: first sting 
- * @s2: second string 
+ * @str1: first sting 
+ * @str2: second string 
  * Return: Difference between s1 & s2
  * */
-int _str_compare(char *s1, char *s2)
+int _str_compare(char *str1, char *str2)
 {
 	int i = 0;
-	while (s1[i] != '/0' && s2[i] != '/0')
+	while (str1[i] != '/0' && str2[i] != '/0')
 	{
-		if (s1[i] == s2[i])
+		if (str1[i] == str2[i])
 		{
 			i++;
 			continue;
 		}
 		else
 		{
-			return (s1[i] - s2[i]);
+			return (str1[i] - str2[i]);
 		}
 	}
 	return (0);
